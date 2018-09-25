74350b1e3b37cf0a66a89ff6852b8d6a9e8b30e1
Scott O'Dea <odeascott@gmail.com>
2018-09-25 14:41:55 +1000

    second update

--- portal.rb
+++ portal.rb
@@ -2,11 +2,17 @@ portalend= false
 
 puts "Find your way home"
 puts "Which room will you enter"
-puts "room 3"
+puts "Room 1 Room 2 Room 3 Room 4"
 
 user_input = gets.chomp
-if user_input == "room 3"
+if user_input == "Room 1"
+end
+if user_input == "Room 2"
+end
+if user_input == "Room 3"
     portalend = true
+end
+if user_input == "Room 4"
 end
 
 if portalend == true
