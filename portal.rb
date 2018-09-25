435057d098d2c2f4897c82c2a430ee6693dd0dcf
Tyson Hofstetter <tysonh652@gmail.com>
2018-09-25 15:15:49 +1000

    update tyson

--- portal.rb
+++ portal.rb
@@ -9,6 +9,9 @@ if user_input == "Room 1"
   puts "a dirty dank room"
 end
 if user_input == "Room 2"
+    puts "you find yourself in a room where there is a donut on a plate and a not telling you not to eat it."
+    puts "Do you eat it?"
+    
 end
 if user_input == "Room 3"
     portalend = true
