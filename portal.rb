280c65220ad0b4728270df2e88af993cbc18dcb1
Tyson Hofstetter <tysonh652@gmail.com>
2018-09-25 15:05:07 +1000

    upadte branch

--- portal.rb
+++ portal.rb
@@ -2,11 +2,18 @@ portalend= false
 
 puts "Find your way home"
 puts "Which room will you enter"
-puts "room 3"
+puts "Room 1 Room 2 Room 3 Room 4"
 
 user_input = gets.chomp
-if user_input == "room 3"
+if user_input == "Room 1"
+  puts "a dirty dank room"
+end
+if user_input == "Room 2"
+end
+if user_input == "Room 3"
     portalend = true
+end
+if user_input == "Room 4"
 end
 
 if portalend == true
